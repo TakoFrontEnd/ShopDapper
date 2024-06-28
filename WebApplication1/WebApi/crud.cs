a8a9b8283e68af939807471b741a33e2d7f61ccc
klas9802 <klas9806@gmail.com>
2024-06-28 16:38:19 +0800

    first commit

---
+++ WebApplication1/WebApi/crud.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using YourNamespace.Models;
+
+namespace WebApplication1.WebApi
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CrudController : ControllerBase
+    {
+        private readonly string _connectString;
+
+        public CrudController(IConfiguration configuration)
+        {
+            _connectString = configuration.GetConnectionString("Northwind");
+        }
+
+        public void TryConnect()
+        {
+            using (var conn = new SqlConnection(_connectString))
+            {
+                try
+                {
+                    conn.Open();
+                    Console.WriteLine("Connection successful!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Connection failed: {ex.Message}");
+                }
+            }
+        }
+        
+        
+        [HttpGet]
+        public IEnumerable<Order> GetOrders()
+        {
+            using (var conn = new SqlConnection(_connectString))
+            {
+                return conn.Query<Order>("SELECT * FROM Orders");
+            }
+        }
+
+        public Order GetOrderId(int OrderID)
+        {
+            var sql = @"SELECT * FROM Orders WHERE OrderID = @OrderID";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("OrderID", OrderID);
+
+            using (var conn = new SqlConnection(_connectString))
+            {
+                var result = conn.QueryFirstOrDefault<Order>(sql, parameters);
+                return result;
+            }
+        }
+        
+        
+        
+    }
+}
---
+++ WebApplication1/wwwroot/js/shop.js
@@ -0,0 +1,46 @@
+﻿const { createApp, ref, reactive, onMounted } = Vue;
+
+createApp({
+    setup(){
+        let orderList = reactive([]);
+        let temp = reactive([]);
+        const OrderID = ref('');
+
+        const getOrderList = () => {
+            axios.get('api/crud/GetOrders')
+                .then(response => response.data)
+                .then(getData => {
+                    //console.log("原本:",getData);
+                    orderList.splice(0, orderList.length, ...getData);
+                    // console.log("test : ",orderList[0].orderId)
+                })
+                .catch(error => {
+                    console.log('Error fetching data:', error);
+                });
+        };
+        
+        const getOrderId = (OrderID) => {
+            axios.get('api/crud/GetOrderId',{
+                params:{OrderID : OrderID}
+            })
+                .then(response => response.data)
+                .then(getData => {
+                    orderList.splice(0, orderList.length, getData);
+                })
+                .catch(error => {
+                    console.log('Error fetching data:', error);
+                });
+        };
+        
+        onMounted(()=>{
+            getOrderList();
+        });
+        
+        return {OrderID, orderList, getOrderId};
+        
+        
+    }
+}).mount('#app');
+
+git config user.name "klas9802"
+git config user.email "klas9806@gmail.com"
